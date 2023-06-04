928f866eb66b30dfaa5949e203682b10a1c3949f
jiyoon-lee <jiyoon3421@gmail.com>
2023-06-04 18:06:07 +0900

    웹페이지 호스팅

---
+++ src/App.ts
@@ -0,0 +1,7 @@
+type Props = {
+  root: HTMLDivElement
+}
+
+export default class App {
+  constructor(private props: Props) { }
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import App from './App.js';
+
+new App({
+  root: document.querySelector('#app')! as HTMLDivElement
+})
